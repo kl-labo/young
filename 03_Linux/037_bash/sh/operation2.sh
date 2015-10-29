53757636897f42ff41f660999a3321dc5ce138eb
smith05 <tatsuya05sumi@gmail.com>
2015-10-29 23:33:29 +0900

    プルリク時の指摘事項の修正

--- 03_Linux/037_bash/sh/operation2.sh
+++ 03_Linux/037_bash/sh/operation2.sh
@@ -4,30 +4,30 @@ a=`expr 1 + 1`
 
 #等しい
 if [ "$a" -eq 2 ]; then
-  echo "変数aは2と等しいよ"
+  echo "変数aは2と等しい"
 fi
 
 #等しくない
 if [ "$a" -ne 1 ]; then
- echo "変数aは2と等しくないよ"
+ echo "変数aは2と等しくない"
 fi
 
 #より大きい
 if [ "$a" -gt 1 ]; then
- echo "変数aは1より大きいよ"
+ echo "変数aは1より大きい"
 fi
 
 #より小さい
 if [ "$a" -lt 3 ]; then
- echo "変数aは2より小さいよ"
+ echo "変数aは2より小さい"
 fi
 
 #以上(等しいも含むため、あえて同一の値で)
 if [ "$a" -ge 2 ]; then
- echo "変数aは2以上だよ"
+ echo "変数aは2以上"
 fi
 
 #以下(等しいも含むため、あえて同一の値で)
 if [ "$a" -le 2 ]; then
- echo "変数aは2以下だよ"
+ echo "変数aは2以下"
 fi
